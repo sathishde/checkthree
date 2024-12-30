f80c91cc8db4ebb6a9e9176221642dbe724b1c99
Sathish Devarajulu <sathish.devarajulu@gmail.com>
2024-12-30 10:46:43 +0300

    new br create

---
+++ src/test/java/com/qa/tests/Login2test.java
@@ -0,0 +1,16 @@
+package com.qa.tests;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class Login2test {
+
+    @Test
+    public void login2test(){
+        System.out.println("SUMMER IS");
+        int a = 10;
+        int b = 20;
+        Assert.assertEquals(30, a+b);
+    }
+
+}
